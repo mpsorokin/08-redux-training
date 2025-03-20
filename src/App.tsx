6b266483a7b61e0039a9023ce570f191fb3c0261
Aa <aa@example.com>
2025-03-20 15:37:50 +0200

    added 2 counters

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { DecrementAction, IncrementAction, store } from "./store.ts";
+import {CounterId, DecrementAction, IncrementAction, store} from "./store.ts";
 import {useEffect, useReducer} from "react";
 
 
 function App() {
-// hack for component force update
-const [, forceUpdate] = useReducer(x => x + 1, 0);
 
-useEffect(() => {
-    const unSubscribe = store.subscribe(() => {
-        // function passed here calls each time action fires
-        forceUpdate();
-    });
-
-    return unSubscribe;
-}, [])
   return (
     <>
       <div>
@@ -28,23 +18,43 @@ useEffect(() => {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-          counter: {store.getState().counter}
-        <button onClick={() => store.dispatch({ type: "increment" } satisfies IncrementAction)}>
-          Increment
-        </button>
-          <button onClick={() => store.dispatch({ type: "decrement" } satisfies DecrementAction)}>
-              Decrement
-          </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+        <Counter counterId="first" />
+        <Counter counterId="second" />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
   )
+}
+
+export function Counter({counterId}: {counterId: CounterId}) {
+    // hack for component force update
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
+
+    useEffect(() => {
+        const unSubscribe = store.subscribe(() => {
+            // function passed here calls each time action fires
+            forceUpdate();
+        });
+
+        return unSubscribe;
+    }, [])
+    return (
+        <>
+            <div className="card">
+                counter: {store.getState().counters[counterId]?.counter}
+                <button onClick={() => store.dispatch({ type: "increment", payload: {counterId} } satisfies IncrementAction)}>
+                    Increment
+                </button>
+                <button onClick={() => store.dispatch({ type: "decrement", payload: {counterId} } satisfies DecrementAction)}>
+                    Decrement
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+        </>
+    )
 }
 
 export default App
